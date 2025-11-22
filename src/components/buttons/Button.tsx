38235ac9607d7031a486bab544dba42b1333f100
Luca <Lucafunk2002@googlemail.com>
2025-11-22 20:31:09 +0100

    feat: fixed light mode colors

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -21,17 +21,24 @@ export default function Button(props: ButtonProps) {
     ? props.functional === "submit"
       ? "bg-functional-submit"
       : "bg-functional-delete"
-    : "bg-primary-600 dark:bg-primary-800"
+    : "bg-gray-50 dark:bg-primary-800" // <— DEIN Weiß!
+
   const flex = props.arrowRight ? "flex-1" : ""
   const textLeft = props.textLeft ? "justify-start" : "justify-center"
+
   return (
     <TouchableOpacity
       onPress={props.onPress}
       className={`flex-row ${textLeft} items-center ${flex} relative ${bg} rounded-lg p-4 gap-3`}
     >
       {props.icon && (
-        <Ionicons name={props.icon} size={16} color={colors.gray[50]} />
+        <Ionicons
+          name={props.icon}
+          size={16}
+          color={colors.gray[900]} // dark text on white
+        />
       )}
+
       {props.emojiWithBackground && (
         <EmojiWithBackground
           color={props.emojiWithBackground.color}
@@ -39,15 +46,17 @@ export default function Button(props: ButtonProps) {
           size='xs'
         />
       )}
-      <Text className='text-gray-50 font-semibold rounded-lg'>
+
+      <Text className='text-gray-900 dark:text-gray-50 font-semibold rounded-lg'>
         {props.title}
       </Text>
+
       {props.arrowRight && (
         <View className='absolute right-4'>
           <Ionicons
             name={"chevron-forward"}
             size={16}
-            color={colors.gray[50]}
+            color={colors.gray[900]}
           />
         </View>
       )}
