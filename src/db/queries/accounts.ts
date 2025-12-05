9d2340bed872b2190a750fc59be2fe309b773f6a
thePHANT <kevke.kunze@gmail.com>
2025-12-05 18:54:25 +0100

    wip: update account queries

--- src/db/queries/accounts.ts
+++ src/db/queries/accounts.ts
@@ -9,28 +9,60 @@ export default function useAccounts() {
   const [error, setError] = useState<Error | null>(null)
   const [loading, setLoading] = useState(false)
 
-  const get = async () => {
+  const get = async ({ id }: { id: number }) => {
     setLoading(true)
     setError(null)
     try {
       return db
-        .select()
+        .select({
+          id: accountTable.id,
+          name: accountTable.name,
+          color: accountTable.color,
+          emoji: accountTable.emoji,
+          balance: accountTable.balance,
+        })
         .from(accountTable)
-        .where(eq(accountTable.name, "Default"))
+        .where(eq(accountTable.id, id))
         .limit(1)
-        .then((result) => result[0].name)
+        .then((result) => result[0])
     } catch (err) {
       const error =
         err instanceof Error ? err : new Error("Unknown error occurred")
       setError(error)
-      console.error("Error fetching categories:", error)
+      console.error("Error fetching account:", error)
       return null
     } finally {
       setLoading(false)
     }
   }
+
+  const getMany = async () => {
+    setLoading(true)
+    setError(null)
+    try {
+      return db
+        .select({
+          id: accountTable.id,
+          name: accountTable.name,
+          color: accountTable.color,
+          emoji: accountTable.emoji,
+          balance: accountTable.balance,
+        })
+        .from(accountTable)
+    } catch (err) {
+      const error =
+        err instanceof Error ? err : new Error("Unknown error occurred")
+      setError(error)
+      console.error("Error fetching accounts:", error)
+      return []
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return {
     get,
+    getMany,
     error,
     loading,
   }
