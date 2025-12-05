e4c39a3f03a4d1e14df09d28b07afd4599cc6c36
thePHANT <kevke.kunze@gmail.com>
2025-12-05 19:42:40 +0100

    wip: add create, update query for account

--- src/db/queries/accounts.ts
+++ src/db/queries/accounts.ts
@@ -1,3 +1,4 @@
+import type { CustomColorKeys } from "@/assets/colors"
 import { eq } from "drizzle-orm"
 import { useState } from "react"
 import { useDb } from ".."
@@ -60,9 +61,89 @@ export default function useAccounts() {
     }
   }
 
+  const create = async ({
+    name,
+    color,
+    emoji,
+    balance,
+    currencyId = 1,
+  }: {
+    name: string
+    color: CustomColorKeys
+    emoji: string
+    balance: number
+    currencyId?: number
+  }) => {
+    setLoading(true)
+    setError(null)
+    try {
+      const result = await db
+        .insert(accountTable)
+        .values({
+          name,
+          color,
+          emoji,
+          balance,
+          currencyId,
+        })
+        .returning()
+
+      return result[0]
+    } catch (err) {
+      const error =
+        err instanceof Error ? err : new Error("Unknown error occurred")
+      setError(error)
+      console.error("Error creating account:", error)
+      return null
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const update = async ({
+    id,
+    name,
+    color,
+    emoji,
+    balance,
+  }: {
+    id: number
+    name: string
+    color: CustomColorKeys
+    emoji: string
+    balance: number
+  }) => {
+    setLoading(true)
+    setError(null)
+    try {
+      const result = await db
+        .update(accountTable)
+        .set({
+          name,
+          color,
+          emoji,
+          balance,
+        })
+        .where(eq(accountTable.id, id))
+        .returning()
+
+      return result[0]
+    } catch (err) {
+      const error =
+        err instanceof Error ? err : new Error("Unknown error occurred")
+      setError(error)
+      console.error("Error updating account:", error)
+      return null
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return {
     get,
     getMany,
+    create,
+    update,
     error,
     loading,
   }
